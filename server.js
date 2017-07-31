443cfd7624e0169b8eeddff4a3936448df989a1f
Maximus Wu <stib108@yahoo.com.tw>
2017-07-31 14:22:10 +0800

    solidify the structure

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
 
 const config = require('./dataConfig');
 const setupController = require('./controllers/setupController');
 const apiController = require('./controllers/apiController');
-
-const app = express();
 
 mongoose.connect(config.getDbConnectionString());
 
 setupController(app);
 apiController(app);
 
-//app.use('/assets', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.sendFilr(path.join(__dirname, '/src/index.js'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port);
