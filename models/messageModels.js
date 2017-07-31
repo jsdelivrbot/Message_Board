493343c8c1668da1f5847e47e46969bf5acb9098
Maximus Wu <stib108@yahoo.com.tw>
2017-07-31 13:24:05 +0800

    small modification (now use sandbox, not computer as database)

--- models/messageModels.js
+++ models/messageModels.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const messageSchema = new Schema({
-	id: String,
-	mComment: String,
-	content: Array,
-});
+const messageSchema = new Schema({ id: String, mComment: String, content: Array });
 
 const Messages = mongoose.model('Messages', messageSchema);
 
